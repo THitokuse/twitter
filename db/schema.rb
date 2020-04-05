c0b05c4fb15e51195d076337198d88fa2aa8a63d
天野彰吾 <1200asmac@amanoshougonoMacBook-Pro.local>
2020-04-05 17:12:31 +0900

    make index file

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_31_161312) do
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
